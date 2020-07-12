7120d90fbaeed9a2e9694d1de0e30f801c193f03
Henrique Bispo dos Santos <henrique.santos56@fatec.sp.gov.br>
2020-07-12 15:05:12 -0300

    Atividade 3

---
+++ Atividade3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Atividade3
+{
+    partial class formMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblVeiculos = new System.Windows.Forms.DataGridView();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.btPesquisar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblVeiculos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblVeiculos
+            // 
+            this.tblVeiculos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblVeiculos.Location = new System.Drawing.Point(15, 38);
+            this.tblVeiculos.Name = "tblVeiculos";
+            this.tblVeiculos.Size = new System.Drawing.Size(316, 150);
+            this.tblVeiculos.TabIndex = 0;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(12, 15);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(132, 13);
+            this.lblMarca.TabIndex = 1;
+            this.lblMarca.Text = "Digite a marca do veículo:";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(150, 12);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(100, 20);
+            this.txtMarca.TabIndex = 2;
+            // 
+            // btPesquisar
+            // 
+            this.btPesquisar.Location = new System.Drawing.Point(256, 10);
+            this.btPesquisar.Name = "btPesquisar";
+            this.btPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btPesquisar.TabIndex = 3;
+            this.btPesquisar.Text = "Pesquisar";
+            this.btPesquisar.UseVisualStyleBackColor = true;
+            this.btPesquisar.Click += new System.EventHandler(this.btPesquisar_Click);
+            // 
+            // formMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 199);
+            this.Controls.Add(this.btPesquisar);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.tblVeiculos);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formMarca";
+            this.ShowIcon = false;
+            this.Text = "Pesquisa de veículos por marca";
+            ((System.ComponentModel.ISupportInitialize)(this.tblVeiculos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tblVeiculos;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Button btPesquisar;
+    }
+}
+
---
+++ Atividade3/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Atividade3
+{
+    public partial class formMarca : Form
+    {
+        private SqlConnection conexao;
+        private SqlCommand comando;
+        private string sqlStr, strconexao;
+        public formMarca()
+        {
+            InitializeComponent();
+            txtMarca.Focus();
+        }
+
+        private void btPesquisar_Click(object sender, EventArgs e)
+        {
+            string marca = txtMarca.Text.Trim();
+            if (marca.Length == 0)
+            {
+                MessageBox.Show("Por favor, informe o nome da marca!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMarca.Focus();
+                return;
+            }
+
+            strconexao = "Server=localhost; Database=AgenciaCarrosQBraQBra; Integrated Security=True;";
+            conexao = new SqlConnection(strconexao);
+
+            sqlStr = "SELECT * FROM Veiculos WHERE Marca LIKE @Marca";
+            comando = new SqlCommand(sqlStr, conexao);
+
+            comando.Parameters.Add("@Marca", SqlDbType.Text);
+            comando.Parameters["@Marca"].Value = marca;
+
+            try
+            {
+                conexao.Open();
+                SqlDataAdapter consulta = new SqlDataAdapter(comando);
+                DataSet dados = new DataSet();
+                consulta.Fill(dados);
+
+                tblVeiculos.DataSource = dados.Tables[0];
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
